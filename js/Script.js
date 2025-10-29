e94039225803d76e2fd6974e2303dc6d1d5a564e
brick <djc939@gmail.com>
2025-10-29 17:18:57 -0500

    Delete js directory

--- js/Script.js
+++
@@ -1,5 +0,0 @@
-function Test() {
-  //const Random = Math.Random() * 100 + 1;
-  //document.getElementById("message").innerHTML = toString(Random);
-  document.getElementById("message").innerHTML = "Hello, world"
-}
